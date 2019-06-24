163964c63931f18f4c7fdac6622d5c57f503b81d
Nick Xitco <nick.xitco@appdynamics.com>
2019-06-23 21:50:09 -0700

    Fixed the playhead snapping and overall finetuned the timeline

--- timeline.js
+++ timeline.js
@@ -2,29 +2,53 @@ let playhead_dragging = false;
 let zoomFactor = 0; //This could be a confusing variable but what I'm thinking is 0 for still, 1 for the right, -1 for the left? we'll see, could be dumb
 let panning = false;
 let pzcs_downX; //pan zoom control surface
-
-let timelineZoom = 1;
-let timelinePosition = 0;
+let playhead_downX;
 
 
-playhead.onmousedown = function () {
+const baselineZoom = 4;
+
+let clientWidth = timeline.clientWidth;
+
+let timelineLength = 900;
+let timelineZoom = baselineZoom;
+let scrollbarLeft = 0;
+let scrollbarRight = timeline.clientWidth / timelineZoom;
+let scrollbarLength = scrollbarRight - scrollbarLeft;
+
+let playhead_position = 160;
+
+rightPanZoomHandle.setAttribute('cx', scrollbarRight);
+barPanZoom.setAttribute('x2', scrollbarRight);
+setPatternPosition();
+setPlayheadPosition();
+
+function setPatternPosition() {
+    pattern.setAttribute('patternTransform', "scale("+ timelineZoom * (timeline.clientWidth / timelineLength) + ", 1) translate(" + -scrollbarLeft * (timelineLength / timeline.clientWidth) + ")");
+
+}
+
+function setPlayheadPosition() {
+    playhead.setAttribute('d', "M" + (playhead_position * (timelineZoom * (timeline.clientWidth / timelineLength)) - (scrollbarLeft * timelineZoom))  +  " 10 l -5 -5 v -5 h 10 v 5 l -5 5 v 40");
+
+}
+
+playhead.onmousedown = function (e) {
     playhead_dragging = true;
+    playhead_downX = e.offsetX;
 };
 
 timeline.onmousemove = function (e){
     if (playhead_dragging === true) {
-        playhead.setAttribute('d', "M" + parseInt(e.offsetX + timelinePosition) +  " 10 l -5 -5 v -5 h 10 v 5 l -5 5 v 40")
+        playhead_position += (e.offsetX - playhead_downX) / (timelineZoom * (timeline.clientWidth / timelineLength));
+        playhead_downX = e.offsetX;
+        setPlayheadPosition();
     }
 };
 
 
-//TODO also trigger this on timeline.onmouseup and mouseleave
 playhead.onmouseup = function () {
     playhead_dragging = false;
 };
-
-
-
 
 
 rightPanZoomHandle.onmousedown = function (e) {
@@ -38,8 +62,7 @@ leftPanZoomHandle.onmousedown = function (e) {
 };
 
 
-//TODO this is probably too small a surface to be checking, consider expanding all the way up to the document scale.
-panZoomControlSurface.onmousemove = function (e) {
+timelineDiv.onmousemove = function (e) {
     if (zoomFactor !== 0) {
         zoomPZCS((e.offsetX - pzcs_downX) * zoomFactor);
         pzcs_downX = e.offsetX;
@@ -55,6 +78,11 @@ barPanZoom.onmousedown = function(e) {
     panning = true;
 };
 
+document.onmouseup = function () {
+    stopZoom();
+    stopPan();
+    playhead_dragging = false;
+};
 
 panZoomControlSurface.onmouseup = function () {
     stopZoom();
@@ -73,30 +101,33 @@ function stopPan() {
 
 
 function zoomPZCS(x) {
-    if (parseInt(leftPanZoomHandle.getAttribute('cx')) - x >= 0) {
-        leftPanZoomHandle.setAttribute('cx', (parseInt(leftPanZoomHandle.getAttribute('cx')) - x).toString());
-        barPanZoom.setAttribute('x1', (parseInt(barPanZoom.getAttribute('x1')) -  x).toString());
-        timelineZoom -= (x/50);
+    if (scrollbarLength + 2*x > 20) {
+        if (parseInt(leftPanZoomHandle.getAttribute('cx')) - x >= 0) {
+            leftPanZoomHandle.setAttribute('cx', (parseInt(leftPanZoomHandle.getAttribute('cx')) - x).toString());
+            barPanZoom.setAttribute('x1', (parseInt(barPanZoom.getAttribute('x1')) -  x).toString());
+            scrollbarLeft -= x;
+        }
+        if (parseInt(rightPanZoomHandle.getAttribute('cx')) + x <= timeline.clientWidth) {
+            rightPanZoomHandle.setAttribute('cx', parseInt(rightPanZoomHandle.getAttribute('cx')) + x);
+            barPanZoom.setAttribute('x2', parseInt(barPanZoom.getAttribute('x2')) + x);
+            scrollbarRight += x;
+        }
+        scrollbarLength = scrollbarRight - scrollbarLeft;
+        timelineZoom = timeline.clientWidth/scrollbarLength;
+        setPatternPosition();
+        setPlayheadPosition()
     }
-    if (parseInt(rightPanZoomHandle.getAttribute('cx')) + x <= timeline.clientWidth - 40) {
-        rightPanZoomHandle.setAttribute('cx', parseInt(rightPanZoomHandle.getAttribute('cx')) + x);
-        barPanZoom.setAttribute('x2', parseInt(barPanZoom.getAttribute('x2')) + x);
-        timelineZoom -= (x/50);
-    }
-    //TODO cap zoom
-    pattern.setAttribute('patternTransform', "scale("+ timelineZoom + ", 1) translate(" + -timelinePosition + ")");
-    //TODO lock playhead to this transformation as well..... somehow
-    //playheads horizontal movement should be correlated to the zoom factor in that: zoomed out=smaller movement, zoomedin=larger movement.
 }
 
 function shiftPZCS(x) {
-    if (parseInt(leftPanZoomHandle.getAttribute('cx')) + x >= 0 && parseInt(rightPanZoomHandle.getAttribute('cx')) + x <= (timeline.clientWidth - 40)) {
+    if (parseInt(leftPanZoomHandle.getAttribute('cx')) + x >= 0 && parseInt(rightPanZoomHandle.getAttribute('cx')) + x <= (timeline.clientWidth)) {
         rightPanZoomHandle.setAttribute('cx', parseInt(rightPanZoomHandle.getAttribute('cx')) + x);
         leftPanZoomHandle.setAttribute('cx', parseInt(leftPanZoomHandle.getAttribute('cx')) + x);
         barPanZoom.setAttribute('x1', parseInt(barPanZoom.getAttribute('x1')) + x);
         barPanZoom.setAttribute('x2', parseInt(barPanZoom.getAttribute('x2')) + x);
-        timelinePosition += x;
-        pattern.setAttribute('patternTransform', "scale("+ timelineZoom + ", 1) translate(" + -timelinePosition + ")");
-        playhead.setAttribute('transform', "translate(" + -timelinePosition + ")");
+        scrollbarLeft += x;
+        scrollbarRight += x;
+        setPlayheadPosition();
+        setPatternPosition();
     }
 }
